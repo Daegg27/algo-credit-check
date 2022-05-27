7f52099004d82f1bf74f65dcf63c76c4854f8471
Dalton Eggleston <egglestondalton@yahoo.com>
2022-05-27 08:00:59 -0700

    almost finished python

--- python/credit_check.py
+++ python/credit_check.py
@@ -1,8 +1,26 @@
-def credit_check(str):
-    pass
+def credit_check(inputPara):
+    
+    str_list = list(inputPara)
+    final_sum = 0
+    used_number = len(str_list)
+    
+    
+    for prop in str_list:
+            if int(prop) * used_number > 9:
+                multipled_number = int(prop) * used_number
+                multipled_number = str(multipled_number)
+                split_num = []
+                split_num.append(multipled_number[0])
+                split_num.append(multipled_number[1])
+                final_sum += int(split_num[0]) + int(split_num[1])
+                
 
-# Your Luhn Algorithm Here
-# Expected Output:
-# If it is valid, print "The number is valid!"
-# If it is invalid, print "The number is invalid!"
+    if final_sum % 10 == 0:
+        return "The number is valid!"
+    else:
+        return "The number is invalid!"
 
+   
+
+
+# print(credit_check("32325939"))
